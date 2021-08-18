959741d3496543b43330dc6331fea12f834b1964
Katerina <kostiukkaterina@gmail.com>
2021-08-18 09:37:32 +0300

    init: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let num = n.toString();
+    let result = ''; 
+    
+    if (n < 0) {
+      for (let i = num.length - 1; i > 0; i--) {
+        result = result + num[i];
+      }
+    } else {
+      for (let i = num.length - 1; i >= 0; i--) {
+        result = result + num[i];
+      }
+    } 
+    return +result;
 }
